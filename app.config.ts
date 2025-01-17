130f976d1700f2089f33efd87b44f4d601042f17
6chinwei <6chinwei@gmail.com>
2025-01-17 15:37:06 +0800

    Add an app config for contact list

--- app.config.ts
+++ app.config.ts
@@ -9,10 +9,35 @@ const SKILLS = [
 
 export default defineAppConfig({
   appUrl: process.env.APP_URL || '',
+
   seo: {
     title: 'Rocky Liu (@6chinwei) - A Web-Tech Pathfinder transforming challenges into elegant solutions',
     description: `Hello! I’m Rocky Liu (@6chinwei), a Web-Tech Pathfinder transforming challenges into elegant solutions. Specializing in ${ SKILLS.join(' / ') }`,
     keywords: `Rocky Liu, 6chinwei, Full-Stack Developer, Web-Tech Pathfinder, Elegant Web Solutions, ${ SKILLS.join(', ') }`,
   },
-  skills: SKILLS
+
+  skills: SKILLS,
+
+  contactList: [
+    {
+      type: 'codepen',
+      text: 'Codepen',
+      url: 'https://codepen.io/6chinwei',
+    },
+    {
+      type: 'linkedin',
+      text: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/6chinwei/',
+    },
+    {
+      type: 'github',
+      text: 'Github',
+      url: 'https://github.com/6chinwei',
+    },
+    {
+      type: 'mail',
+      text: 'Mail',
+      url: 'mailto:6chinwei@gmail.com',
+    },
+  ]
 });
