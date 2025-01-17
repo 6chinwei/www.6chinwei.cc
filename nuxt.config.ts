9a9493f85b40dfdd8a22e9afcdb7920afcd9bd1c
6chinwei <6chinwei@gmail.com>
2025-01-17 11:24:39 +0800

    Update the config for Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint'],
   devtools: { enabled: true },
-  compatibilityDate: '2024-11-01'
-})
+  compatibilityDate: '2024-11-01',
+  app: {
+    head: {
+      link: [
+        // Google Fonts for Montserrat
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap' },
+      ],
+    },
+  },
+});
