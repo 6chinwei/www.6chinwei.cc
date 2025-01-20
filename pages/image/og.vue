851e959a04b3128628e9efb3ae6a2ad7a420d239
6chinwei <6chinwei@gmail.com>
2025-01-20 15:23:45 +0800

    Add a image generator for OG image

---
+++ pages/image/og.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="override">
+    <IndexPage />
+  </div>
+</template>
+
+<script setup lang="ts">
+import IndexPage from '~/pages/index.vue';
+</script>
+
+<style lang="scss">
+html {
+  font-size: 31px;
+}
+
+.override {
+  .page {
+    background-image: none;
+    background-color: var(--color-blue);
+  }
+
+  .container {
+    max-width: unset;
+    width: unset;
+    padding: 0;
+  }
+
+  .app-window {
+    width: 1200px;
+    height: 630px;
+  }
+
+  .title {
+    margin-bottom: 1.25rem;
+  }
+
+  .skills {
+    display: none;
+  }
+
+  .find-me {
+    display: none;
+  }
+
+  .contact li[class^="link-"] {
+    background-image: none;
+    --size: 2.5rem;
+  }
+}
+</style>
